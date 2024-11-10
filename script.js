fb7df97883d5e7801ea627d9cacd3172efc5f6ca
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-10 16:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('ageForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let age = document.querySelector('#age')
+    let name = document.querySelector('#name')
+    const ageError = document.querySelector('#ageError')
+    const nameError = document.querySelector('#nameError')
+
+    ageError.textContent = ''
+    nameError.textContent = ''
+
+    let isValid = true
+
+    if (!age.value) {
+      ageError.textContent = 'Age is required.'
+      isValid = false
+    }
+
+    if (!name.value) {
+      nameError.textContent = 'Name is required.'
+      isValid = false
+    }
+
+    if (!isValid) return
+
+    age = parseInt(age.value, 10)
+    name = name.value
+
+    const ageCheckPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve()
+        } else {
+          reject()
+        }
+      }, 4000)
+    })
+      .then(() => {
+        alert(`Welcome, ${name}. You can vote.`)
+      })
+      .catch(() => {
+        alert(`Oh sorry ${name}. You aren't old enough.`)
+      })
+  })
+})
